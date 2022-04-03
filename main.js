8bc1b5b6f88b055fbf6710174839a30243c9f79b
WANG LIAO <z5306312@ad.unsw.edu.au>
2022-04-03 16:53:07 +0800

    basically finished front-end part

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const modal_wrapper = document.querySelector('.modals-wrapper');
-if (modal_wrapper) {
-    function display_modal(modal_id) {
-        const modal = document.getElementById(modal_id);
-        modal.style.display = "flex";
-        modal_wrapper.style.display = "flex";
 
-        const close_button = document.getElementById("close-modal");
-        close_button.addEventListener('click', () => {
-            modal.style.display = "none";
-            modal_wrapper.style.display = "none";
-        })
-    }
+function display_modal(modal_id) {
+    const modal = document.getElementById(modal_id);
+    modal.style.display = "flex";
+    modal_wrapper.style.display = "flex";
+
+    const close_button = document.getElementById("close-modal");
+    close_button.addEventListener('click', () => {
+        modal.style.display = "none";
+        modal_wrapper.style.display = "none";
+    })
 }
 
 const copies = document.querySelectorAll(".copy");
